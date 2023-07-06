fa5ff64ac534555a8be134aa0ba87a387cfe16db
JustKooba <jakubsakw2009@gmail.com>
2023-07-06 23:27:40 +0200

    added the login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 import "./App.css";
+import logo from "./trello-assets/logo.jpg";
 
-function App() {}
+function LoginScreen() {
+  return (
+    <div className="login-screen">
+      <img src={logo} alt="logo" />
+      <input type="text" placeholder="Login" className="login-input" />
+      <input type="password" placeholder="Password" className="login-input" />
+      <button className="sign-in-btn">Sign in</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <LoginScreen />
+    </>
+  );
+}
 
 export default App;
