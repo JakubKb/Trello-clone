b726f6eea98859749f4c1c76c2fde9d5177229ab
JustKooba <jakubsakw2009@gmail.com>
2023-07-06 23:32:41 +0200

    changed the logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import "./App.css";
-import logo from "./trello-assets/logo.jpg";
 
 function LoginScreen() {
   return (
     <div className="login-screen">
-      <img src={logo} alt="logo" />
+      <h1>Trello list</h1>
       <input type="text" placeholder="Login" className="login-input" />
       <input type="password" placeholder="Password" className="login-input" />
       <button className="sign-in-btn">Sign in</button>
