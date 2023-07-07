83e23737e0885a627dddc21ad2e83998375e4ac1
JustKooba <jakubsakw2009@gmail.com>
2023-07-07 11:55:15 +0200

    added test login data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import "./App.css";
+import { useState } from "react";
 
 function LoginScreen() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const checkLogin = (password, login) => {
+    if (password === "test123" && login === "test11") {
+      console.log("Logged in!");
+    }
+  };
+
   return (
     <div className="login-screen">
       <h1>Trello list</h1>
-      <input type="text" placeholder="Login" className="login-input" />
-      <input type="password" placeholder="Password" className="login-input" />
-      <button className="sign-in-btn">Sign in</button>
+      <input
+        type="text"
+        placeholder="Login"
+        className="login-input"
+        value={login}
+        onChange={(e) => setLogin(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="login-input"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        className="sign-in-btn"
+        onClick={() => checkLogin(password, login)}
+      >
+        Sign in
+      </button>
     </div>
   );
 }
